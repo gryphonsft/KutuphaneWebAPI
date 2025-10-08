acdacb3ffcd093f14a345eff61f25dfb8940ec30
gryphonsft <abdbzd01@gmail.com>
2025-10-08 16:42:52 +0300

    BookRepository

--- WebApiProjesi.Infrastructure/Repositories/BookRepository.cs
+++ WebApiProjesi.Infrastructure/Repositories/BookRepository.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApiProjesi.Domain.Entities;
+using WebApiProjesi.Domain.Interfaces;
+using WebApiProjesi.Infrastructure.Data;
 
 namespace WebApiProjesi.Infrastructure.Repositories
 {
-    public class BookRepository
+    public class BookRepository : IBookRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Book>> GetAllAsync() => await _context.Book.ToListAsync();
+        public async Task<Book?> GetByIdAsync(int id) => await _context.Book.FindAsync(id);
+        public async Task AddAsync(Book book)
+        {
+            await _context.Book.AddAsync(book);
+        }
+        public async Task UpdateAsync(Book book)
+        {
+            _context.Book.Update(book);
+        }
+        public async Task DeleteByIdAsync(int id)
+        {
+            var book = await GetByIdAsync(id);
+            if (book != null)
+            {
+                _context.Book.Remove(book);
+            }
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
     }
 }
